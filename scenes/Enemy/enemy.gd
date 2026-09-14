bb375255239ac5f258f2741935caa3b8cd9a3f0d
Allan Sechrist <sechristallan@gmail.com>
2026-09-14 19:49:27 +0900

    Add enemy data resource.

--- scenes/Enemy/enemy.gd
+++ scenes/Enemy/enemy.gd
@@ -1,6 +1,12 @@
 extends Area2D
 class_name Enemy
 
+@export var data: EnemyData
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+func _ready() -> void:
+	if data:
+		sprite_2d.texture = data.sprite
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
