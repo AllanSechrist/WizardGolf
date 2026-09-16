09d1f851ea07f0ea2e3d250404876764370e01fc
Allan Sechrist <sechristallan@gmail.com>
2026-09-16 19:11:27 +0900

    update project settings, add debug for hud

--- scenes/Enemy/enemy.gd
+++ scenes/Enemy/enemy.gd
@@ -2,6 +2,9 @@ extends Area2D
 class_name Enemy
 
 @export var data: EnemyData
+#DEBUG
+@export var hud: HUD # TODO: change to signal
+#END DEBUG
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
@@ -10,4 +13,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
+		hud.on_score_changed(1) #DEBUG TODO: Update to signal manager. 
 		queue_free()
